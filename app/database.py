92a26ab22ff05b5b1df2df4c54429f37bb223e56
kaviyaM019 <you@example.com>
2025-03-02 13:41:55 +0530

    4.50hrs

---
+++ app/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHMEY_DATABASE_URL = 'postgresql://postgres:password123@localhost/fastapi'
+
+engine = create_engine(SQLALCHMEY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException , Depends
 from fastapi.params import Body
 
 from pydantic import BaseModel
@@ -7,11 +7,26 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
 
+from . import models
+from .database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 
 class Post(BaseModel):
     title: str
@@ -20,7 +35,7 @@ class Post(BaseModel):
 while True:
     try:
         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
-        password='Kaviyaa@321', cursor_factory=RealDictCursor)
+        password='password123', cursor_factory=RealDictCursor)
         cursor = conn.cursor()
         print("Database connection was succesfull!")
         break
@@ -54,7 +69,10 @@ def root():
     return {"message": "Hello World"}
 
 
-
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return{"status": "success"}
+    
 
 @app.get("/posts")
 def get_posts():
@@ -87,12 +105,15 @@ def get_posts(id: str):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id :int):
-    index = find_index_post(id)
-    if index == None:
+
+    cursor.execute("""DELETE FROM posts WHERE id = %s returning *""",(str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
          detail=f"post with id:{id} does not exist")
 
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
@@ -100,12 +121,15 @@ def delete_post(id :int):
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
-    if index == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-         detail=f"post with id:{id} does not exist")
 
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"data": post_dict}
+    cursor.execute("""UPDATE posts SET title = %s, content =%s, published = %s  WHERE id = %s
+    RETURNING *""",
+    (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+         detail=f"post with id: {id} does not exist")
+
+    
+    return {"data": updated_post}
